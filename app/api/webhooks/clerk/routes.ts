335e599e1f09249ccd6aa226b1e969e7b4e3f48b
Moses Mativo <clishtivo@gmail.com>
2024-01-24 16:47:33 +0300

    webhook issue

--- app/api/webhooks/clerk/routes.ts
+++ app/api/webhooks/clerk/routes.ts
@@ -53,37 +53,9 @@ export async function POST(req: Request) {
   // Get the ID and type
   const { id } = evt.data;
   const eventType = evt.type;
-
-  if(eventType === 'user.created'){
-    const {id, email_addresses, image_url, first_name, last_name, username} = evt.data;
-
-    const user = {
-        clerkId: id,
-        email: email_addresses[0].email_address,
-        username: username!,
-        firstName: first_name,
-        lastName: last_name,
-        photo: image_url,
-    }
-
-    const newUser = await createUser(user);
-
-    // if the new user exists
-
-    if(newUser){
-      await clerkClient.users.updateUserMetadata(id, {
-        publicMetadata:{
-          userId: newUser._id
-        }
-      })
-    }
-
-    return NextResponse.json({message: 'OK', user: newUser});
-
-  }
-
-  if(eventType === 'user.updated'){
-    const {id, email_addresses, image_url, first_name, last_name, username} = evt.data;
+ 
+  if(eventType === 'user.created') {
+    const { id, email_addresses, image_url, first_name, last_name, username } = evt.data;
 
     const user = {
       clerkId: id,
@@ -94,18 +66,41 @@ export async function POST(req: Request) {
       photo: image_url,
     }
 
-  const updatedUser = await updateUser(id, user);
+    const newUser = await createUser(user);
 
-  return NextResponse.json({message: 'OK', user: updatedUser});
+    if(newUser) {
+      await clerkClient.users.updateUserMetadata(id, {
+        publicMetadata: {
+          userId: newUser._id
+        }
+      })
+    }
 
+    return NextResponse.json({ message: 'OK', user: newUser })
   }
 
-  if(eventType === 'user.deleted'){
-    const {id} = evt.data;
+  if (eventType === 'user.updated') {
+    const {id, image_url, first_name, last_name, username } = evt.data
+
+    const user = {
+      firstName: first_name,
+      lastName: last_name,
+      username: username!,
+      photo: image_url,
+    }
+
+    const updatedUser = await updateUser(id, user)
+
+    return NextResponse.json({ message: 'OK', user: updatedUser })
+  }
+
+  if (eventType === 'user.deleted') {
+    const { id } = evt.data
+
     const deletedUser = await deleteUser(id!)
-    return NextResponse.json({message: 'OK', user: deletedUser});
-  }
 
+    return NextResponse.json({ message: 'OK', user: deletedUser })
+  }
+ 
   return new Response('', { status: 200 })
 }
- 
