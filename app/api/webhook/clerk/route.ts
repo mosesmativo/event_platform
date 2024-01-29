c180a588aa50b90e4520146bca849e8ac210793e
Moses Mativo <clishtivo@gmail.com>
2024-01-29 15:49:18 +0300

    user created test

--- app/api/webhook/clerk/route.ts
+++ app/api/webhook/clerk/route.ts
@@ -1,6 +1,7 @@
 import { Webhook } from 'svix'
 import { headers } from 'next/headers'
 import { WebhookEvent } from '@clerk/nextjs/server'
+import { createUser, deleteUser, updateUser } from '@/lib/actions/user.actions'
  
 export async function POST(req: Request) {
  
@@ -48,11 +49,23 @@ export async function POST(req: Request) {
   }
  
   // Get the ID and type
-  const { id } = evt.data;
+  // const { id } = evt.data;
   const eventType = evt.type;
- 
-  console.log(`Webhook with and ID of ${id} and type of ${eventType}`)
-  console.log('Webhook body:', body)
+
+  if(eventType === 'user.created'){
+    const {id, email_addresses, image_url, first_name, last_name, username } = evt.data;
+
+    const user = {
+      clerkId: id,
+      email: email_addresses[0].email_address,
+      username: username!,
+      firstName: first_name,
+      lastName: last_name,
+      photo: image_url,
+    }
+    
+    console.log(`the user created is: ${user}`);
+  }
  
   return new Response('', { status: 200 })
 }
