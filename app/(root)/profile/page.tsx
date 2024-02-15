baf5586472cda1044f1b723807ddcb347f4a752f
Moses Mativo <clishtivo@gmail.com>
2024-02-15 22:09:16 +0300

    check out button

---
+++ app/(root)/profile/page.tsx
@@ -0,0 +1,68 @@
+import Collections from '@/components/shared/Collections';
+import { Button } from '@/components/ui/button';
+import { getEventsByUser } from '@/lib/actions/event.actions';
+import { auth } from '@clerk/nextjs/server';
+import Link from 'next/link';
+import React from 'react'
+
+async function ProfilePage() {
+
+    const { sessionClaims } = auth();
+    const userId = sessionClaims?.userId as string;
+
+    const organizedEvents = await getEventsByUser({userId, page: 1});
+
+  return (
+    <>
+    {/* My Tickets */}
+    <section className='bg-primary-50 bg-dotted-pattern bg-cover bg-center py-5 md:py-10'>
+        <div className="wrapper flex items-center justify-center md:justify-between">
+            <h3 className='h3-bold text-center sm:text-left'>My Tickets</h3>
+            <Button asChild className='button hidden sm:flex'>
+                <Link href="/#events" >
+                    Explore More Events
+                </Link>
+            </Button>
+        </div>
+    </section>
+    {/* <section className='wrapper my-8'>
+    <Collections
+          data={events?.data}
+          emptyTitle="No Events Ticket Purchase yet"
+          emptyStateSubtext="No worries plenty of exiting events to explore"
+          collectionType="My_Events"
+          limit={3}
+          page={1}
+          urlParamName='ordersPage'
+          totalPages={2}
+    />
+    </section> */}
+    {/* Event organized */}
+    <section className='bg-primary-50 bg-dotted-pattern bg-cover bg-center py-5 md:py-10'>
+        <div className="wrapper flex items-center justify-center md:justify-between">
+            <h3 className='h3-bold text-center sm:text-left'>Events Organized</h3>
+            <Button asChild className='button hidden sm:flex'>
+                <Link href="/events/create" >
+                    Create New Event
+                </Link>
+            </Button>
+        </div>
+    </section>
+
+    <section className='wrapper my-8'>
+    <Collections
+          data={organizedEvents?.data}
+          emptyTitle="No Events Have Been created yet"
+          emptyStateSubtext="Go Create one now"
+          collectionType="Events_Organized"
+          limit={6}
+          page={1}
+          urlParamName='eventsPage'
+          totalPages={2}
+    />
+    </section>
+    </>
+  )
+}
+
+export default ProfilePage;
---
+++ components/shared/CheckoutButton.tsx
@@ -0,0 +1,11 @@
+import { IEvent } from '@/lib/database/models/event.model'
+import React from 'react'
+
+
+function CheckoutButton({event}: { event: IEvent}) {
+  return(
+    <div>CheckoutButton</div>
+  )
+}
+
+export default CheckoutButton
