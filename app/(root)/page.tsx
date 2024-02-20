e302fd6a603c07f85b7486bf875b043089471487
Moses Mativo <clishtivo@gmail.com>
2024-02-20 22:46:13 +0300

    profile page

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,49 +1,63 @@
-import Collections from '@/components/shared/Collections'
-import { Button } from '@/components/ui/button'
-import { getAllEvents } from '@/lib/actions/event.actions'
-import Image from 'next/image'
-import Link from 'next/link'
+import Collections from "@/components/shared/Collections";
+import { ExternalLink } from "lucide-react";
+import { Button } from "@/components/ui/button";
+import { getAllEvents } from "@/lib/actions/event.actions";
+import Image from "next/image";
+import Link from "next/link";
 
 export default async function Home() {
   const events = await getAllEvents({
-    query: '',
-    category: '',
+    query: "",
+    category: "",
     page: 1,
     limit: 6,
-  })
+  });
 
   return (
     <>
-      <section className='bg-primary-50 bg-dotted-pattern bg-contain py-5 md:py-10'>
-        <div className='wrapper grid grid-cols-1 gap-5 md:grid-cols-2 2xl:gap-0'>
+      <section className="bg-primary-50 bg-dotted-pattern bg-contain py-3 md:py-6">
+        <div className="wrapper grid grid-cols-1 gap-5 md:grid-cols-2 2xl:gap-0">
           <div className="flex flex-col justify-center gap-8">
-            <h1 className='h1-bold'>
-                Host, Connect, Celebrate: Your Events, Our Platform!
+            <h1 className="text-2xl font-bold leading-loose text-gray-900 sm:text-4xl ">
+              Host, Connect, Celebrate: Your Events, Our Platform!
             </h1>
-            <p className="p-regular-20 md:p-regular-18">Book and learn helpful tips from 3,168+ mentors in world-class companies with our global community.</p>
-            <Button size="lg" asChild className='button w-full sm:w-fit'>
-              <Link href="#events">
-                Explore
+            <p className="p-regular-20 md:p-regular-18">
+              Book and learn helpful tips from 3,168+ mentors in world-class
+              companies with our global community.
+            </p>
+            <Button
+              size="lg"
+              asChild
+              className="button w-full sm:w-fit p-medium-12">
+              <Link href="#events" className="flex items-center gap-2">
+                Explore <ExternalLink />
               </Link>
             </Button>
           </div>
-          <Image 
-            src="/assets/images/hero.png" 
-            alt='hero banner'
+          <Image
+            src="/assets/images/hero.png"
+            alt="hero banner"
             width={1000}
             height={1000}
-            className='max-h-[70vh] object-contain object-center 2xl:max-h-[50vh]'
+            className="max-h-[70vh] object-contain object-center 2xl:max-h-[50vh]"
           />
         </div>
-      </section> 
+      </section>
 
-      <section id='events' className='wrapper my-8 flex flex-col gap-8 md:gap-12'>
-        <h2 className='h2-bold'> Trusted By <br/> Thousands of Events</h2>
+      <section
+        id="events"
+        className="wrapper my-8 flex flex-col gap-8 md:gap-12">
+        <h2 className="h2-bold">
+          {" "}
+          Trusted By <br /> Thousands of Events
+        </h2>
 
         <div className="flex w-full flex-col-gap-5 md:flex-row">
           Search <br />
-          Category<br /> 
-          Filter<br />
+          Category
+          <br />
+          Filter
+          <br />
         </div>
 
         <Collections
@@ -57,5 +71,5 @@ export default async function Home() {
         />
       </section>
     </>
-  )
+  );
 }
--- app/(root)/profile/page.tsx
+++ app/(root)/profile/page.tsx
@@ -1,31 +1,28 @@
-import Collections from '@/components/shared/Collections';
-import { Button } from '@/components/ui/button';
-import { getEventsByUser } from '@/lib/actions/event.actions';
-import { auth } from '@clerk/nextjs/server';
-import Link from 'next/link';
-import React from 'react'
+import Collections from "@/components/shared/Collections";
+import { Button } from "@/components/ui/button";
+import { getEventsByUser } from "@/lib/actions/event.actions";
+import { auth } from "@clerk/nextjs/server";
+import Link from "next/link";
+import React from "react";
 
 async function ProfilePage() {
+  const { sessionClaims } = auth();
+  const userId = sessionClaims?.userId as string;
 
-    const { sessionClaims } = auth();
-    const userId = sessionClaims?.userId as string;
-
-    const organizedEvents = await getEventsByUser({userId, page: 1});
+  const organizedEvents = await getEventsByUser({ userId, page: 1 });
 
   return (
     <>
-    {/* My Tickets */}
-    <section className='bg-primary-50 bg-dotted-pattern bg-cover bg-center py-5 md:py-10'>
+      {/* My Tickets */}
+      <section className="bg-primary-50 bg-dotted-pattern bg-cover bg-center py-5 md:py-10">
         <div className="wrapper flex items-center justify-center md:justify-between">
-            <h3 className='h3-bold text-center sm:text-left'>My Tickets</h3>
-            <Button asChild className='button hidden sm:flex'>
-                <Link href="/#events" >
-                    Explore More Events
-                </Link>
-            </Button>
+          <h3 className="h3-bold text-center sm:text-left">My Tickets</h3>
+          <Button asChild className="button hidden sm:flex">
+            <Link href="/#events">Explore More Events</Link>
+          </Button>
         </div>
-    </section>
-    {/* <section className='wrapper my-8'>
+      </section>
+      {/* <section className='wrapper my-8'>
     <Collections
           data={events?.data}
           emptyTitle="No Events Ticket Purchase yet"
@@ -37,32 +34,30 @@ async function ProfilePage() {
           totalPages={2}
     />
     </section> */}
-    {/* Event organized */}
-    <section className='bg-primary-50 bg-dotted-pattern bg-cover bg-center py-5 md:py-10'>
+      {/* Event organized */}
+      <section className="bg-primary-50 bg-dotted-pattern bg-cover bg-center py-5 md:py-10">
         <div className="wrapper flex items-center justify-center md:justify-between">
-            <h3 className='h3-bold text-center sm:text-left'>Events Organized</h3>
-            <Button asChild className='button hidden sm:flex'>
-                <Link href="/events/create" >
-                    Create New Event
-                </Link>
-            </Button>
+          <h3 className="h3-bold text-center sm:text-left">Events Organized</h3>
+          <Button asChild className="button hidden sm:flex">
+            <Link href="/events/create">Create New Event</Link>
+          </Button>
         </div>
-    </section>
+      </section>
 
-    <section className='wrapper my-8'>
-    <Collections
+      <section className="wrapper my-8">
+        <Collections
           data={organizedEvents?.data}
           emptyTitle="No Events Have Been created yet"
           emptyStateSubtext="Go Create one now"
           collectionType="Events_Organized"
           limit={6}
           page={1}
-          urlParamName='eventsPage'
+          urlParamName="eventsPage"
           totalPages={2}
-    />
-    </section>
+        />
+      </section>
     </>
-  )
+  );
 }
 
 export default ProfilePage;
