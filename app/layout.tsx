064ae7b53eb71a7bac6a464c2244c388a05878f3
Moses Mativo <clishtivo@gmail.com>
2024-02-21 14:37:03 +0300

    Dark Mode

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,33 +1,40 @@
-import type { Metadata } from 'next'
-import { Poppins } from 'next/font/google'
-import './globals.css'
-import { ClerkProvider } from '@clerk/nextjs';
+import type { Metadata } from "next";
+import { Poppins } from "next/font/google";
+import "./globals.css";
+import { ClerkProvider } from "@clerk/nextjs";
+import { ThemeProvider } from "@/components/theme-provider";
 
-const poppins = Poppins({ 
-  subsets: ['latin'], 
-  weight: ['400', '500', '600', '700'],
-  variable: '--font-poppins',
-
+const poppins = Poppins({
+  subsets: ["latin"],
+  weight: ["400", "500", "600", "700"],
+  variable: "--font-poppins",
 });
 
 export const metadata: Metadata = {
-  title: 'Evently',
-  description: 'Platform for even management',
+  title: "Evently",
+  description: "Platform for even management",
   icons: {
-    icon: '/assets/images/logo.svg'
-  }
-}
+    icon: "/assets/images/logo.svg",
+  },
+};
 
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
   return (
     <ClerkProvider>
       <html lang="en">
-        <body className={poppins.variable}>{children}</body>
+        <body className={poppins.variable}>
+          <ThemeProvider
+            attribute="class"
+            defaultTheme="Light"
+            disableTransitionOnChange>
+            {children}
+          </ThemeProvider>
+        </body>
       </html>
     </ClerkProvider>
-  )
+  );
 }
--- components/shared/Header.tsx
+++ components/shared/Header.tsx
@@ -4,35 +4,43 @@ import Link from "next/link";
 import { Button } from "../ui/button";
 import Navitems from "./Navitems";
 import MobileNav from "./MobileNav";
+import { DarkMode } from "./DarkMode";
 
 function Header() {
   return (
     <header className="w-full border-b">
-        <div className="wrapper flex items-center justify-between">
+      <div className="wrapper flex items-center justify-between">
         <Link href="/" className="w-36">
-            <Image src="/assets/images/logo.svg" width={128} height={38} alt="logo"/>
+          <Image
+            src="/assets/images/logo.svg"
+            width={128}
+            height={38}
+            alt="logo"
+          />
         </Link>
 
         <SignedIn>
-            <nav className="md:flex-between hidden w-full max-w-xs">
-                <Navitems />
-            </nav>
+          <nav className="md:flex-between hidden w-full max-w-xs">
+            <Navitems />
+          </nav>
         </SignedIn>
-
         <div className="flex w-32 justify-end">
-            <SignedIn>
-                <UserButton afterSignOutUrl="/" />
-                <MobileNav />
-            </SignedIn>
-            <SignedOut>
-                <Button asChild className="rounded-full" size="lg">
-                    <Link href="/sign-in">Login</Link>
-                </Button>
-            </SignedOut>
+          <SignedIn>
+            <UserButton afterSignOutUrl="/" />
+            <MobileNav />
+          </SignedIn>
+          <SignedOut>
+            <Button asChild className="rounded-full" size="lg">
+              <Link href="/sign-in">Login</Link>
+            </Button>
+          </SignedOut>
+          <div className="pl-2">
+            <DarkMode />
+          </div>
         </div>
-        </div>
+      </div>
     </header>
-  )
+  );
 }
 
 export default Header;
